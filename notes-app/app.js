364cc7d20773b89877c5bf1fc615759dfa9c55d3
Augusto <augustoamaro@icloud.com>
2021-11-21 22:21:54 -0300

    Adicionado seção 4

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,60 @@
-const chalk = require('chalk')
+const chalk = require('chalk');
+const { argv } = require('yargs');
+const yargs = require('yargs');
 const notes = require('./notes')
 
+// Customizar versão do yargs
+yargs.version('1.1.0')
+
+// Criação do comando Add
+yargs.command({
+    command: 'add',
+    describe: 'Adicionar uma nova nota',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string' 
+        }
+    },
+    handler: function (argv) {
+        console.log(`Title: ${argv.title}`)
+        console.log(`Body: ${argv.body}`)
+    }
+})
+
+// Criação do comando Remove
+yargs.command({
+    command: 'remove',
+    describe: 'Remove uma nova nota',
+    handler: function () {
+        console.log(`Title: ${argv.title}`)
+    }
+})
 
 
+// Criação do comando Read
+yargs.command({
+    command: 'read',
+    describe: 'Ler uma nova nota',
+    handler: function () {
+        console.log('Lendo uma nova nota');
+    }
+})
 
+// Criação do comando List
+yargs.command({
+    command: 'list',
+    describe: 'Listar todas as notas',
+    handler: function () {
+        console.log('Listando todas as notas');
+    }
+})
 
-const message = notes()
-console.log(message);
-
-
-const chalkMsg = chalk.green.inverse.bold('Success!')
-console.log(chalkMsg);
+console.log(yargs.argv);
 
